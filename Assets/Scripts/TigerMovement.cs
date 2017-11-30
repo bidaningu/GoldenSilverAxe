d58594de9dcaf7adb2f44c343d27a8e5ef8a007d
cspark <bidaningu@gmail.com>
2017-11-30 20:53:29 +0900

    Animal Tiger Scene Complete

--- Assets/Scripts/TigerMovement.cs
+++ Assets/Scripts/TigerMovement.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 public class TigerMovement : MonoBehaviour 
 {
 	public Transform animalStopPoint;
+	public Transform fadeInOutPoint;
+
 	private UnityEngine.AI.NavMeshAgent nav;
-	
+
 	private void Start() {
 		nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		FadeInOutStatus.fadeInOutStatus = true;
 	}
 
 	private void Update() {
-		nav.SetDestination(animalStopPoint.position);
+		Debug.Log("Status : " + FadeInOutStatus.fadeInOutStatus);
+
+		if(FadeInOutStatus.fadeInOutStatus == true)
+		{
+			nav.SetDestination(animalStopPoint.position);
+		}
+
+		else
+		{
+			nav.SetDestination(fadeInOutPoint.position);
+
+			if ((transform.position - fadeInOutPoint.position).magnitude < 2f)
+			{
+				Destroy(gameObject);
+			}
+			//Vector3.Distance();
+				
+		}
 	}
 }
